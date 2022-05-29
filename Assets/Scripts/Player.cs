bea1ae152df06771ccb25863b9a4b420789b3e5f
Mitchell Philipp <mgpalpha1@gmail.com>
2022-05-29 17:27:39 -0400

    add basic aiming

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,8 +9,13 @@ public class Player : MonoBehaviour
     private PlayerInput _input;
     private Rigidbody2D _rb;
     private Animator _an;
+    [SerializeField] private CinemachineCameraOffset _cco;
+
+    private Vector2 moveDir;
+    private Vector2 aimDir;
 
     [SerializeField] private float baseSpeed = 3f;
+    [SerializeField] private float maxAimOffset = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +28,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_input.currentActionMap["Move"].ReadValue<Vector2>());
+        moveDir = _input.currentActionMap["Move"].ReadValue<Vector2>();
+        if (_input.currentControlScheme == "Gamepad") aimDir = _input.currentActionMap["Gamepad Aim"].ReadValue<Vector2>();
+        else  {
+            
+        }
+        Debug.Log(aimDir);
+        if (_cco) {
+            _cco.m_Offset = Vector2.ClampMagnitude(aimDir, 1) * maxAimOffset;
+        }
+        _an.SetFloat("facingX", aimDir.x);
+        _an.SetFloat("facingY", aimDir.y);
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveDir = _input.currentActionMap["Move"].ReadValue<Vector2>();
-        Debug.Log(moveDir);
         _rb.velocity = Vector2.ClampMagnitude(moveDir, 1) * baseSpeed;
-        _an.SetFloat("facingX", moveDir.x);
-        _an.SetFloat("facingY", moveDir.y);
     }
 }
