47079d9ebb63425bbdb5ece7ec84642e79cc00a1
Mitchell Philipp <mgpalpha1@gmail.com>
2022-07-25 18:58:03 -0400

    add weapon spread settings

--- Assets/Scripts/ScriptableObjects/Weapon.cs
+++ Assets/Scripts/ScriptableObjects/Weapon.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Festival/Weapon")]
 public class Weapon : ScriptableObject
 {
+    [SerializeField] private List<WeaponBurst> bursts;
+    public ReadOnlyCollection<WeaponBurst> Bursts {
+        get => bursts.AsReadOnly();
+    }
+}
+
+[Serializable]
+public class WeaponBurst {
+    [SerializeField] private float startTime = 0;
+    public float StartTime { get => startTime; }
+    
     [SerializeField] private BulletData bullet;
     public BulletData Bullet { get => bullet; }
+
+    [SerializeField] private float firingSpread = 0;
+    public float FiringSpread { get => firingSpread; }
+    [SerializeField] private int bulletsInSpread = 1;
+    public int BulletsInSpread { get => bulletsInSpread; }
+    [SerializeField] private float spreadTimeInterval = 0;
+    public float SpreadTimeInterval { get => spreadTimeInterval; }
+    [SerializeField] private float spreadRandomization = 0;
+    public float SpreadRandomization { get => spreadRandomization; }
+
+    [SerializeField] private float aimOffset = 0;
+    public float AimOffset { get => aimOffset; }
 }
--- Assets/Scripts/WeaponEmitter.cs
+++ Assets/Scripts/WeaponEmitter.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 public class WeaponEmitter : MonoBehaviour
 {
     [SerializeField] private GameObject _bulletPrefab;
     [SerializeField] private LayerMask hitLayers;
+
+    public bool Firing { get; private set; }
     
     // Start is called before the first frame update
     void Start()
@@ -20,8 +23,36 @@ public class WeaponEmitter : MonoBehaviour
     }
 
     public void Fire(Weapon weapon, Vector2 direction) {
+        
+        
+        StartCoroutine(FiringCoroutine(weapon, direction));
+    }
+
+    private BulletController FireNewBullet(BulletData bullet, Vector2 direction) {
         GameObject newBullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
         BulletController controller = newBullet.GetComponent<BulletController>();
-        controller.Init(weapon.Bullet, transform.position, direction, hitLayers);
+        controller.Init(bullet, transform.position, direction, hitLayers);
+        return controller;
+    }
+
+    IEnumerator FiringCoroutine(Weapon weapon, Vector2 direction) {
+        ReadOnlyCollection<WeaponBurst> bursts = weapon.Bursts;
+        foreach (WeaponBurst burst in bursts) {
+            if (burst.StartTime > 0) yield return new WaitForSeconds(burst.StartTime);
+            float localAimAngle = burst.AimOffset;
+            if (burst.BulletsInSpread > 1) localAimAngle -= burst.FiringSpread/2;
+            float angleBetweenBullets = burst.FiringSpread / (burst.BulletsInSpread - (burst.FiringSpread == 360 ? 0 : 1));
+            for (int i = 0; i < burst.BulletsInSpread; i++) {
+                // if (i == 0 && burst.BulletsInSpread > 1 && burst.FiringSpread == 360 && burst.SpreadTimeInterval == 0) continue; 
+                if (burst.SpreadTimeInterval > 0 && i != 0) yield return new WaitForSeconds(burst.SpreadTimeInterval);
+                float thisBulletAimAngle = localAimAngle;
+                if (burst.SpreadRandomization > 0) {
+                    thisBulletAimAngle += Random.Range(-burst.SpreadRandomization/2, burst.SpreadRandomization/2);
+                }
+                Vector2 bulletDirection = direction.Rotate(thisBulletAimAngle);
+                FireNewBullet(burst.Bullet, bulletDirection);
+                if (burst.BulletsInSpread > 1) localAimAngle += angleBetweenBullets;
+            }
+        }
     }
 }
