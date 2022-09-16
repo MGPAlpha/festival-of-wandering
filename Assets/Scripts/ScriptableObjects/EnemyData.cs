204de385dd5c73766fbd024851657468514516aa
Mitchell Philipp <mgpalpha1@gmail.com>
2022-09-16 03:04:23 -0400

    add enemy weapon triggers list with selectable subclasses

--- Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections.ObjectModel;
-
-
+using System.Linq;
 
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Festival/Enemy")]
 public class EnemyData : ScriptableObject {
@@ -11,6 +10,38 @@ public class EnemyData : ScriptableObject {
     public string EnemyName { get => enemyName; }
     [SerializeField] private float moveSpeed = 1f;
     public float MoveSpeed { get => moveSpeed; }
-    [SerializeField] private List<Weapon> weapons;
-    public ReadOnlyCollection<Weapon> Weapons { get => weapons.AsReadOnly(); }
+    [SerializeReference] private List<EnemyAttackChoice> weapons;
+    public ReadOnlyCollection<EnemyAttackChoice> Weapons { get => weapons.AsReadOnly(); }
 }
+
+[System.Serializable]
+public abstract class EnemyAttackChoice {
+    
+    public static System.Type[] Subclasses { get; private set; }
+    
+    static EnemyAttackChoice()
+    {
+        Subclasses = typeof(EnemyAttackChoice).Assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(EnemyAttackChoice))).ToArray();
+    }
+
+    [SerializeField] private Weapon weapon;
+    public Weapon Weapon { get; private set; }
+    public abstract bool ShouldChoose();
+}
+
+[System.Serializable]
+public class AttackChoiceAlways : EnemyAttackChoice {
+    public override bool ShouldChoose() => true;
+}
+
+[System.Serializable]
+public class AttackChoiceChance : EnemyAttackChoice {
+    
+    [SerializeField] private float probability = .5f;
+
+    public override bool ShouldChoose()
+    {
+        return Random.Range(0,1) < probability;
+    }
+}
+
