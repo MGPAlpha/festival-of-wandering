606eb5b0dd9b4fc005110ba578a01bf48a8015ae
Mitchell Philipp <mgpalpha1@gmail.com>
2022-09-26 14:49:29 -0400

    add enemy control state machine

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyState {
+    INIT,
+    MOVE,
+    AIM,
+    FIRE,
+    COOLDOWN
+}
+
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent _nma;
+    private WeaponEmitter _we;
     private GameObject target;
 
     public EnemyData enemyType;
-    
+    EnemyState state;
+    private float stateTime = 0;
+    private float lineOfSightTime = 0;
+    [SerializeField] private LayerMask sightBlockingLayers;
+
     // Start is called before the first frame update
     void Start()
     {
         _nma = GetComponent<NavMeshAgent>();
         _nma.updateRotation = false;
         _nma.updateUpAxis = false;
+        _we = GetComponentInChildren<WeaponEmitter>();
         if (enemyType) Initialize(enemyType);
     }
 
@@ -24,14 +38,77 @@ public class Enemy : MonoBehaviour
     }
 
     void Initialize(EnemyData enemy) {
+        state = EnemyState.INIT;
+        stateTime = 0;
         enemyType = enemy;
-        _nma.speed = enemyType.MoveSpeed;
+        _nma.speed = 0;
         target = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
         _nma.SetDestination(target.transform.position);
+        stateTime += Time.deltaTime;
+        switch (state) {
+            case EnemyState.INIT:
+                if (stateTime > enemyType.InitTime) {
+                    TransitionMove();
+                }
+                break;
+            case EnemyState.MOVE:
+                Vector3 targetDir = target.transform.position - transform.position;
+                RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, (Vector2)targetDir.normalized, targetDir.magnitude, sightBlockingLayers);
+                if (hit.collider) {
+                    lineOfSightTime = 0;
+                } else lineOfSightTime += Time.deltaTime;
+                if (stateTime > enemyType.MoveTime && lineOfSightTime > enemyType.LineOfSightTime) {
+                    TransitionAim();
+                }
+                break;
+            case EnemyState.AIM:
+                if (stateTime > enemyType.AimTime) {
+                    TransitionFire();
+                }
+                break;
+            case EnemyState.FIRE:
+                if (!_we.FiringActive) {
+                    TransitionCooldown();
+                }
+                break;
+            case EnemyState.COOLDOWN:
+                if (stateTime > enemyType.CooldownTime) {
+                    TransitionMove();
+                }
+                break;
+        }
+    }
+
+    private void TransitionMove() {
+        stateTime = 0;
+        state = EnemyState.MOVE;
+        _nma.speed = enemyType.MoveSpeed;
+        lineOfSightTime = 0;
+    }
+
+    private void TransitionAim() {
+        stateTime = 0;
+        state = EnemyState.AIM;
+        _nma.speed = enemyType.SpeedWhileAiming;
+    }
+
+    private void TransitionFire() {
+        stateTime = 0;
+        state = EnemyState.FIRE;
+        _nma.speed = enemyType.SpeedWhileFiring;
+        Weapon weapon = EnemyAttackChoice.ChooseFromList(enemyType.Weapons, this);
+        _we.Fire(weapon, (target.transform.position - transform.position).normalized);
+    }
+
+    private void TransitionCooldown() {
+        stateTime = 0;
+        state = EnemyState.COOLDOWN;
+        _nma.speed = 0;
     }
 }
