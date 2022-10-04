70d50027d131f66574b6fdd9e128147549b83baf
Mitchell Philipp <mgpalpha1@gmail.com>
2022-10-03 21:13:52 -0400

    import edgar pro package

---
+++ Assets/Edgar/Runtime/Grid2D/PlatformerGenerator/PlatformerGeneratorGrid2D.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Edgar.Unity
+{
+    [AddComponentMenu("Edgar/Grid2D/Platformer Generator (Grid2D)")]
+    public class PlatformerGeneratorGrid2D : DungeonGeneratorBaseGrid2D
+    {
+        protected override Func<ITilemapLayersHandlerGrid2D> GetTilemapLayersHandler()
+        {
+            return () => new PlatformerTilemapLayersHandlerGrid2D();
+        }
+    }
+}
