ff9c71fc490bcfb7e39322bd2281db6c88866d5e
Mitchell Philipp <mgpalpha1@gmail.com>
2022-10-13 18:15:59 -0400

    add basic enemy anim system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,6 +17,7 @@ public class Enemy : MonoBehaviour, IDamageable
     private NavMeshAgent _nma;
     private WeaponEmitter _we;
     private CircleCollider2D _cc;
+    private Animator _an;
     private GameObject target;
 
     public EnemyData enemyType;
@@ -28,6 +29,8 @@ public class Enemy : MonoBehaviour, IDamageable
     private int health;
     private int maxHealth;
 
+    private bool attackAnimTriggered;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +40,7 @@ public class Enemy : MonoBehaviour, IDamageable
         _nma.updateUpAxis = false;
         _we = GetComponentInChildren<WeaponEmitter>();
         _cc = GetComponent<CircleCollider2D>();
+        _an = GetComponent<Animator>();
         if (enemyType) Initialize(enemyType);
     }
 
@@ -48,10 +52,13 @@ public class Enemy : MonoBehaviour, IDamageable
         state = EnemyState.INIT;
         stateTime = 0;
         enemyType = enemy;
+        _an.runtimeAnimatorController = enemy.AnimatorController;
         health = enemy.BaseHealth;
         maxHealth = enemy.BaseHealth;
+        attackAnimTriggered = false;
         _nma.speed = 0;
         _cc.enabled = true;
+        _an.speed = 1;
         target = GameObject.Find("Player");
     }
 
@@ -67,6 +74,9 @@ public class Enemy : MonoBehaviour, IDamageable
     void Update()
     {
         target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        Vector2 nmaDirection = (_nma.steeringTarget - transform.position).normalized;
+        Vector2 targetDirection = (target.transform.position - transform.position).normalized;
+        Debug.Log(nmaDirection.normalized);
         _nma.SetDestination(target.transform.position);
         stateTime += Time.deltaTime;
         switch (state) {
@@ -76,8 +86,9 @@ public class Enemy : MonoBehaviour, IDamageable
                 }
                 break;
             case EnemyState.MOVE:
-                Vector3 targetDir = target.transform.position - transform.position;
-                RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, (Vector2)targetDir.normalized, targetDir.magnitude, sightBlockingLayers);
+                _an.SetFloat("facingX", nmaDirection.x);
+                _an.SetFloat("facingY", nmaDirection.y);
+                RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, (Vector2)targetDirection.normalized, targetDirection.magnitude, sightBlockingLayers);
                 if (hit.collider) {
                     lineOfSightTime = 0;
                 } else lineOfSightTime += Time.deltaTime;
@@ -86,6 +97,12 @@ public class Enemy : MonoBehaviour, IDamageable
                 }
                 break;
             case EnemyState.AIM:
+                _an.SetFloat("facingX", targetDirection.x);
+                _an.SetFloat("facingY", targetDirection.y);
+                if (!attackAnimTriggered && enemyType.AimTime - stateTime < enemyType.AttackAnimLeadTime) {
+                    _an.SetTrigger("attack");
+                    attackAnimTriggered = true;
+                }
                 if (stateTime > enemyType.AimTime) {
                     TransitionFire();
                 }
@@ -109,6 +126,7 @@ public class Enemy : MonoBehaviour, IDamageable
         stateTime = 0;
         state = EnemyState.MOVE;
         _nma.speed = enemyType.MoveSpeed;
+        _an.SetBool("walking", true);
         lineOfSightTime = 0;
     }
 
@@ -116,10 +134,15 @@ public class Enemy : MonoBehaviour, IDamageable
         stateTime = 0;
         state = EnemyState.AIM;
         _nma.speed = enemyType.SpeedWhileAiming;
+        _an.SetBool("walking", false);
+        attackAnimTriggered = false;
     }
 
     private void TransitionFire() {
         stateTime = 0;
+        if (!attackAnimTriggered) {
+            _an.SetTrigger("attack");
+        }
         state = EnemyState.FIRE;
         _nma.speed = enemyType.SpeedWhileFiring;
         Weapon weapon = EnemyAttackChoice.ChooseFromList(enemyType.Weapons, this);
@@ -130,6 +153,7 @@ public class Enemy : MonoBehaviour, IDamageable
         stateTime = 0;
         state = EnemyState.COOLDOWN;
         _nma.speed = 0;
+        attackAnimTriggered = false;
     }
 
     private void Die() {
@@ -137,5 +161,6 @@ public class Enemy : MonoBehaviour, IDamageable
         state = EnemyState.DEAD;
         _nma.speed = 0;
         _cc.enabled = false;
+        _an.speed = 0;
     }
 }
