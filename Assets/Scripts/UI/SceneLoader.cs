68865cd33594ba1b96e4fe6390e09f12c0003660
reverienest <reverienest@gmail.com>
2022-10-14 07:23:43 -0400

    Completed Main Menu

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public string sceneName;
+
+   public void LoadStage() {
+       SceneManager.LoadScene(sceneName);
+   }
+}
