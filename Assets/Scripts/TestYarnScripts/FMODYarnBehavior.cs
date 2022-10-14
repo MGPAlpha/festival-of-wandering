310f76d3b416201377a8d85f48b584605febe9b0
reverienest <reverienest@gmail.com>
2022-10-14 16:02:27 -0400

    Saving Changes

--- Assets/Scripts/TestYarnScripts/FMODYarnBehavior.cs
+++ Assets/Scripts/TestYarnScripts/FMODYarnBehavior.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class FMODYarnBehavior : MonoBehaviour
 {
     [SerializeField]
 	private GameObject fManager;
 	private FMODUnity.StudioEventEmitter emitter;
+    private FMOD.Studio.EventInstance instance;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +27,41 @@ public class FMODYarnBehavior : MonoBehaviour
 		{
 			Debug.LogWarning("No fmod emitter found, audio will not play.");
 		}
+        instance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Stages/Stage Theme 2");
+        instance.start();
 	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        FindTrackManager();
     }
 
     
-	void findTrackManager()
+	void FindTrackManager()
     {
 		if (!fManager)
         {
 			fManager = GameObject.Find("TrackManager");
 			emitter = fManager.GetComponent<FMODUnity.StudioEventEmitter>();
 		}
+    }
+
+    [YarnCommand("play_track")]
+    void Playtrack(string name) {
+        Debug.Log("WHAT");
+		instance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/" + name);
+        instance.start();
+    }
+
+    void SetParameter(FMOD.Studio.EventInstance e, string name, float value) {
+	    e.setParameterByName(name, value);
+	}
+
+    [YarnCommand("stop_track")]
+    void StopTrack() {
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        instance.release();
+        instance.clearHandle();
     }
 }
