7029518c54f1e648f303f8c45517e0149132633a
Mitchell Philipp <mgpalpha1@gmail.com>
2022-11-19 15:17:04 -0500

    make first combat room create barriers properly

--- Assets/Scripts/RoomArea.cs
+++ Assets/Scripts/RoomArea.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class RoomArea : MonoBehaviour
 {
 
     public static bool InCombat {get; private set;} = false;
+
+    [SerializeField] private bool autoStartWithPlayer = true;
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,31 @@ public class RoomArea : MonoBehaviour
 
     private GameObject[] mists;
 
+    [YarnCommand("start_room")]
+    public void StartRoom() {
+        roomStarted = true;
+
+        InCombat = true;
+        
+        foreach (EnemySpawner spawn in spawners) {
+            spawn.Spawn(Random.Range(.5f, 2f));
+        }
+
+        GameObject mist1 = Instantiate(mistPrefab, transform.position + (transform.lossyScale.y/2 + 1) * Vector3.up, Quaternion.identity);
+        GameObject mist2 = Instantiate(mistPrefab, transform.position + (transform.lossyScale.y/2 + 1) * Vector3.down, Quaternion.identity);
+        GameObject mist3 = Instantiate(mistPrefab, transform.position + (transform.lossyScale.x/2 + 1) * Vector3.left, Quaternion.identity);
+        GameObject mist4 = Instantiate(mistPrefab, transform.position + (transform.lossyScale.x/2 + 1) * Vector3.right, Quaternion.identity);
+        mist1.transform.localScale = new Vector3(transform.lossyScale.x, 2, 1);
+        mist2.transform.localScale = new Vector3(transform.lossyScale.x, 2, 1);
+        mist3.transform.localScale = new Vector3(2, transform.lossyScale.y, 1);
+        mist4.transform.localScale = new Vector3(2, transform.lossyScale.y, 1);
+        mists = new GameObject[]{mist1, mist2, mist3, mist4};
+        foreach (GameObject mist in mists) {
+            MistBarrier mb = mist.GetComponent<MistBarrier>();
+            mb.SetMistActive(true);
+        }
+    }
+
     /// <summary>
     /// Sent when another object enters a trigger collider attached to this
     /// object (2D physics only).
@@ -32,7 +60,7 @@ public class RoomArea : MonoBehaviour
         if (other.TryGetComponent<EnemySpawner>(out EnemySpawner spawner)) {
             spawners.Add(spawner);
         }
-        if (!roomStarted && other.TryGetComponent<Player>(out Player player)) {
+        if (autoStartWithPlayer && !roomStarted && other.TryGetComponent<Player>(out Player player)) {
             roomStarted = true;
 
             InCombat = true;
