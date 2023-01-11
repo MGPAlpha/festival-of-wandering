504c3afceb7a9e8b6e162e817b27f8936a87b81c
Mitchell Philipp <mgpalpha1@gmail.com>
2023-01-11 00:53:30 -0500

    add button prompts to rest of hud

--- Assets/Scripts/UI/MementoTracker.cs
+++ Assets/Scripts/UI/MementoTracker.cs
@@ -6,12 +6,27 @@ using UnityEngine.UI;
 public class MementoTracker : MonoBehaviour
 {
     [SerializeField] private Image mementoIcon;
+    private CanvasGroup cg;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        cg = mementoIcon.GetComponent<CanvasGroup>();
+    }
+
+    [SerializeField] Color activeColor = new Color(1,1,1,1);
+    [SerializeField] Color disabledColor = new Color(.7f,.7f,.7f,.85f);
+
     public void Update()
     {
         if (PlayerSingleton.PlayerSing.Play.Spell) {
-            mementoIcon.enabled = false;
+            cg.alpha = 1f;
             mementoIcon.sprite = PlayerSingleton.PlayerSing.Play.Spell.DisplaySprite;
+            mementoIcon.color = PlayerSingleton.PlayerSing.Play.MementoChargePercentage >= 1 ? activeColor : disabledColor;
         } 
-        else mementoIcon.enabled = false;
+        else cg.alpha = 0f;
     }
 }
